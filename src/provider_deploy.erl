d3e1d34278e79368ea01339f1f1ed91a96998223
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-06-03 22:17:35 +0200

    test

--- src/provider_deploy.erl
+++ src/provider_deploy.erl
@@ -26,10 +26,12 @@ init(State) ->
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
-    RebarConfig = rebar_config:consult_file("rebar.config"),
-    State1 = rebar_state:new(RebarConfig),
-    Profiles = rebar_state:current_profiles(State1),
-    io:format("Profiles is: ~p~n", State1),
+    %%RebarConfig = rebar_config:consult_file("rebar.config"),
+    %%State1 = rebar_state:new(RebarConfig),
+    %%Profiles = rebar_state:current_profiles(State1),
+    %%io:format("Profiles is: ~p~n", Profiles),
+    Options = rebar_state:command_args(State),
+    io:format("Options are: ~p~n", Optionss),
 
     DeployConfigurations = rebar_config:consult_file("deploy.config"),
     io:format("Deploy configurations are:  ~p~n", [DeployConfigurations]),
